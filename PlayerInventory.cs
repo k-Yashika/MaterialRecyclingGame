5d0adf8146d8d97dd3b3214d575f0cda16ef7caf
Yashika <96520667+k-Yashika@users.noreply.github.com>
2024-01-01 10:36:32 +0530

    added

---
+++ PlayerInventory.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerInventory : MonoBehaviour
+{
+    public int NumberOfItems { get; private set; }
+
+    public UnityEvent<PlayerInventory> OnItemCollected;
+
+    public void ItemsCollected()
+    {
+        NumberOfItems++;
+        OnItemCollected.Invoke(this);
+    }
+}
---
+++ ShowUI.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ShowUI : MonoBehaviour
+{
+    public GameObject uiObj;
+
+    private void Start()
+    {
+        uiObj.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider player)
+    {
+        if (player.gameObject.tag == "Player")
+        {
+            uiObj.SetActive(true);
+            StartCoroutine(WaitAndReload());
+        }
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    IEnumerator WaitAndReload()
+    {
+        yield return new WaitForSeconds(2);
+        ReloadScene();
+    }
+}
